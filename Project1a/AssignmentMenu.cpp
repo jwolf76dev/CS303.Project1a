25e60292ebb2cea2faca1e22c00d75c38dd74ce8
kw531 <katiwilliams531@gmail.com>
2016-02-18 21:54:53 -0600

    Kati and Jay power session, adding in functionality.  Renamed AssignmentList to AssignmentMenu

--- Project1a/AssignmentMenu.cpp
+++ Project1a/AssignmentMenu.cpp
@@ -1,69 +1,95 @@
-#include "AssignmentList.h"
+#include "assignmentMenu.h"
 #include <iostream>
 using namespace std;
 
 //Constructor
-AssignmentList::AssignmentList()
+assignmentMenu::assignmentMenu()
 {
-
 }
+
 //Destructor
-AssignmentList::~AssignmentList()
+assignmentMenu::~assignmentMenu()
 {
-
 }
 
-void AssignmentList::displayAssignments()
+void assignmentMenu::addToList(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList, Date due, string desc, Date assign, assignStatus status) {
+	//New object, i'm sure there is a better way to do this
+	assignment tempAssign(due, desc, assign, status);
+
+	if (status == assigned)
+		assignedList.insert(tempAssign);
+	else
+		completedList.insert(tempAssign);
+}
+
+void assignmentMenu::displayAssignments(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
+	Ordered_List<assignment>::iter iter = assignedList.begin();
 	// Use while loop with iterator to go through lists
 	// Create one list for assigned list
 	// Create one list for completed list
 	// Print out file exactly like the file name
+	//Read out the lists
+	iter = assignedList.begin();
+	cout << "--------Assigned List--------" << endl;
+	while (iter != assignedList.end()) {
+		cout << iter->getDueDate() << ", " << iter->getDescription() << ", " << iter->getAssignDate() << ", " << iter->getStatus() << endl;
+		++iter;
+	}
+
+
+	iter = completedList.begin();
+	cout << "------Completed and Late List----" << endl;
+	while (iter != completedList.end()) {
+		cout << iter->getDueDate() << ", " << iter->getDescription() << ", " << iter->getAssignDate() << ", " << iter->getStatus() << endl;
+		++iter;
+	}
 
 }
 
-void AssignmentList::addAssignment()
+void assignmentMenu::addAssignment(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
-
 	Date assignedDate, dueDate;
-	string desc; 
+	string desc;
 	int statChoice;
 	assignStatus status;
 
 	// Parameters are all the necessary parameters for assigned date (user will use menus). 
 	// Prompt user for the assignedDate in MM/DD/YYYY format. 
-	cout << "When was it assigned?" << endl; 
-	cin >> assignedDate; 
+	cout << "When was it assigned? (MM/DD/YYY)" << endl;
+	cin >> assignedDate;
 
 	// Prompt the user for the dueDate in MM/DD/YYYY format. 
-	cout << "When's the due date?" << endl; 
-	cin >> dueDate; 
-
+	cout << "When's the due date? (MM/DD/YYY)" << endl;
+	cin >> dueDate;
+	cin.ignore(1);
 	// Prompt the user for the description. 
-	cout << "Describe the assignment" << endl; 
-	cin >> desc;
+	cout << "Describe the assignment: " << endl;
+	getline(cin, desc);
 
 	// Prompt the user for the assignedStatus. Maybe put this in the menu()
 	cout << "What's the status?" << endl;
 	cout << "1 - Assigned" << endl
-		<< "2 - Completed" << endl
-		<< "3 - Late" << endl << endl;
+		 << "2 - Completed" << endl
+		 << "3 - Late" << endl << endl;
 	cin >> statChoice;
-	
+
+	// Use a switch menu to get the status
 	switch (statChoice) {
 		case 1: {status = assigned; break; }
 		case 2: {status = completed; break; }
 		case 3: {status = late; break; }
+		default: {status = assigned;  cout << "Incorrect option!" << endl; break; } //TODO: fix me! This is dumb.
 	}
-	// Use a switch menu to get the status
 
+	addToList(assignedList, completedList, dueDate, desc, assignedDate, status);
 	// Check if both dates are valid with the checkformat function and trying the Date class
 	// Check if assigned Date is < due Date
 	// Reprompt if the dates are incorrect - otherwise, break out of this function. 
 
 }
 
-void AssignmentList::editDueDate(Date assignDate, Date newDueDate)
+void assignmentMenu::editDueDate(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
 	// Find assignment first
 	// Prompt the user for a date
@@ -77,7 +103,7 @@ void AssignmentList::editDueDate(Date assignDate, Date newDueDate)
 	cout << "Update complete." << endl;
 }
 
-void AssignmentList::editDescription(Date assignDate, string newDesc)
+void assignmentMenu::editDescription(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
 	// Find assignment first
 	// Prompt the user for a date
@@ -90,7 +116,7 @@ void AssignmentList::editDescription(Date assignDate, string newDesc)
 	cout << "Update complete." << endl;
 }
 
-void AssignmentList::completeAssignment(Date assignDate)
+void assignmentMenu::completeAssignment(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
 	// Prompt for assigned date
 	// Call the checkformat function to make sure that it's a workable date
@@ -104,21 +130,21 @@ void AssignmentList::completeAssignment(Date assignDate)
 
 }
 
-void AssignmentList::listLateAssignments()
+void assignmentMenu::listLateAssignments(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
 	// Search the completed list and count the number of late assignments. 
 	// Display the assignments as we find them. 
 	// Increment the count. 
 }
 
-void AssignmentList::saveLists()
+void assignmentMenu::saveLists(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList)
 {
 	// Overload the output operator
 	// Reading out to old file
 	// Let user know that this program is replacing the old file. 
 }
 
-void AssignmentList::exit()
+void assignmentMenu::exit()
 {
 	// Tell the user that any unsaved work will be lost. 
 	cout << "Any unsaved data will be lost, are you sure(Y/N)?:";
@@ -130,10 +156,30 @@ void AssignmentList::exit()
 	}
 }
 
-bool AssignmentList::checkformat(Date checkDate)
+bool assignmentMenu::checkformat(Date checkDate)
 {
 	// Compare the string and check if the date is right
 	// Use the delimiters to check for proper format
 	// Check for uniqueness in both lists. 
+	/*
+
+
+	//Rando Error Checking - for later, bullshit for now
+	/*if (inDate < temp1.getAssignDate()) //If an input date is before assign date
+	{
+	cout << "Error" << endl;
+	}
+
+	if (tempDueDate <= tempAssignDate) //If DueDate <= AssignDate
+	{
+	cout << "Error" << endl;
+	}
+
+	if (tempAssignDate == temp1.getAssignDate()) //If new assignment date already exists
+	{
+	cout << "Error" << endl;
+	}
+
+	*/
 	return 0;
 }
--- Project1a/tempMain.cpp
+++ Project1a/tempMain.cpp
@@ -1,86 +1,33 @@
 #include "assignment.h"
 #include "Ordered_List.h"
+#include "assignmentMenu.h"
 #include <iostream>
 #include <fstream>
-
-
 using namespace std;
-//This is just random stuff.  I was playing around to make sure my class worked.
-void load_data( Ordered_List<assignment>&, Ordered_List<assignment>&);
+
+// funciton declarations
+void load_data(Ordered_List<assignment>&, Ordered_List<assignment>&);
+void main_menu(Ordered_List<assignment>&, Ordered_List<assignment>&);
 string trim(const string& the_string);
+
 void main() {
-	Ordered_List<assignment> completed;
-	Ordered_List<assignment> assigned;
 
-	load_data(completed, assigned);
+	// create lists
+	Ordered_List<assignment> assignedList; // List of "assigned" assignments
+	Ordered_List<assignment> completedList; // List of "completed" and "late" assignments
 
+	// load assignments from file
+	// populate each list as appropriate
+	load_data(assignedList, completedList);
 
-	//Iterators for the lists
-	Ordered_List<assignment>::iter iterA = assigned.begin();
-	Ordered_List<assignment>::iter iterC = completed.begin();
-
-	//Read out the lists
-
-	cout << "Assigned List" << endl;
-	while (iterA != assigned.end()) {
-		cout << iterA->getDueDate()<< ", " << iterA->getDescription() << ", "<< iterA->getAssignDate() << ", " << iterA->getStatus()<< endl;
-		++iterA;
+	// call menu to perform list actions
+	for (;;) {
+		main_menu(assignedList, completedList);
 	}
 
-	cout << "completed/late List" << endl;
-	while (iterC != completed.end()) {
-		cout << iterC->getDueDate() << ", " << iterC->getDescription() << ", " << iterC->getAssignDate() << ", " << iterC->getStatus() << endl;
-		++iterC;
-	}
 	system("pause");
-	
-	/*
-	/*string date = "5-1-2014";
-	Date tempDueDate("4-1-2014");
-	Date tempAssignDate(date);
-	assignStatus temp = assigned;
-
-	int a = 2015, b = 03, c = 23;
-	Date whoCares(a, b, c); //sets date to 03/23/2015
-
-	while (whoCares.check_valid() != 1) {
-
-	}
-	
-	assignment temp1(tempDueDate, tempAssignDate, "things", temp);
-	*/
-
-	//Date rando(2015, 2, 28);
-	//cin >> date;
-	//Date inDate(date);
-	
-	//Rando Error Checking - for later, bullshit for now
-	/*if (inDate < temp1.getAssignDate()) //If an input date is before assign date
-	{
-		cout << "Error" << endl;
-	}
-
-	if (tempDueDate <= tempAssignDate) //If DueDate <= AssignDate
-	{
-		cout << "Error" << endl;
-	}
-
-	if (tempAssignDate == temp1.getAssignDate()) //If new assignment date already exists 
-	{
-		cout << "Error" << endl;
-	}
-
-	cout << "--------------------Main Menu-------------------------------" << endl;
-	cout<< "1 - Load Assignments" << endl
-		<< "2 - Display Assignments" << endl
-		<< "3 - Edit Assignment" << endl
-		<< "4 - Count Late assignments" << endl
-		<< "5 - Add assignment" << endl
-		<< "6 - Mark assignment as complete" << endl
-		<< "7 - Save assignments" << endl
-		<< "8 - Exit"<< endl;*/
-
 }
+
 string trim(const string& the_string)
 {
 	size_t p = the_string.find_first_not_of(" ");
@@ -94,32 +41,31 @@ void load_data(Ordered_List<assignment>& assignedList, Ordered_List<assignment>&
 	string line, dueDate, desc, assignDate, status;
 	ifstream in("assignments.txt");
 	if (in) { // Stream exists.
-		while (getline(in, line)) { //read the next line
+		while (getline(in, line)) { 
+			//Read in the line from the file
 			String_Tokenizer st(line, ","); //parse the tokens in the line (separated by a comma)	
 			dueDate = trim(st.next_token());
 			desc = trim(st.next_token());
 			assignDate = trim(st.next_token());
 			status = trim(st.next_token());
+
+			//Convert tokenized string to date and assignment status
 			Date newDue(dueDate);
 			Date newAssign(assignDate);
 			assignStatus newStatus;
-
+								
+			//Setting the enum
 			if (status == "assigned") {
 				newStatus = assigned;
 			}
-			else if (status == "complete") {
+			else if (status == "completed") {
 				newStatus = completed;
 			}
 			else {
 				newStatus = late;
 			}
 
-			assignment newAssignment(newDue, desc, newAssign, newStatus);
-
-			if (newStatus != assigned)
-				assignedList.insert(newAssignment);
-			else
-				completedList.insert(newAssignment);
+			assignmentMenu::addToList(assignedList, completedList, newDue, desc, newAssign, newStatus);
 		}
 	}
 	in.close();
@@ -127,4 +73,56 @@ void load_data(Ordered_List<assignment>& assignedList, Ordered_List<assignment>&
 
 }
 
+void main_menu(Ordered_List<assignment>& assignedList, Ordered_List<assignment>& completedList) {
+	int task;
+	cout << "-----------------Main Menu-------------------" << endl << endl;
+	cout << "Choose an option:" << endl;
+	cout << "1 - Display assignments" << endl
+		 << "2 - Add an assignment" << endl
+		 << "3 - Edit due date of an assignment" << endl
+		 << "4 - Edit description of an assignment" << endl
+		 << "5 - Complete an assignment" << endl
+		 << "6 - Display late assignments" << endl
+		 << "7 - Save" << endl
+		 << "8 - Exit" << endl << endl;
+	cout << "Your choice: ";
+	cin >> task;
+
+	
+	switch (task) {
+		case 1: {
+			assignmentMenu::displayAssignments(assignedList,completedList);
+			break;
+			}
+		case 2: {
+			assignmentMenu::addAssignment(assignedList, completedList);
+			break;
+		}
+		case 3: {
+			assignmentMenu::editDueDate(assignedList, completedList);
+			break;
+		}
+		case 4: {
+			assignmentMenu::editDescription(assignedList, completedList);
+			break;
+		}
+		case 5: {
+			assignmentMenu::completeAssignment(assignedList, completedList);
+			break;
+		}
+		case 6: {
+			assignmentMenu::listLateAssignments(assignedList, completedList);
+			break;
+		}
+		case 7: {
+			assignmentMenu::saveLists(assignedList, completedList);
+			break;
+		}
+		case 8: {
+			assignmentMenu::exit();
+			break;
+		}
+	
+	}
+}
 
